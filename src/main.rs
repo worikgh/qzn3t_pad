ce4bf5cc47ba222f64b3ee59d41331384325bc42
Worik <5387413+worikgh@users.noreply.github.com>
2025-07-10 12:47:15 +1200

    Imported code from qzn3t/lpx_ctl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,235 @@
-fn main() {
-    println!("Hello, world!-)");
+//! Use a Novation LPX Pad as a musical instrument
+//! Control the colours on the display
+//! Translate the MIDI signals from the raw PAD number from the LPX into
+//! noteon/noteoff signals
+//! On start up connect directly to the LPX (it must exist ad be
+//! available) then set up a virtual connection for the synthesiser
+//! and connect to it later
+//! ["Programmer's Manual" ](https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Launchpad%20X%20-%20Programmers%20Reference%20Manual.pdf)
+extern crate midir;
+extern crate serde;
+// mod lpx_ctl_error;
+mod section;
+
+use crate::midir::os::unix::VirtualOutput;
+use crate::section::Section;
+use midir::{MidiInput, MidiInputConnection, MidiOutput, MidiOutputConnection};
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::result::Result;
+use std::sync::mpsc::{self, Receiver, Sender};
+
+/// Initialise a vector of `Section` from a file.
+fn load_sections(filename: &str) -> Option<Vec<Section>> {
+    let mut file = match File::open(filename) {
+        Ok(f) => f,
+        Err(err) => panic!("{err}"),
+    };
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(_) => (),
+        Err(err) => panic!("{err}"),
+    };
+
+    // Create the sections from the file
+    let mut sections: Vec<Section> = Section::parse_json(&content).expect("Failed parsing JSON");
+    // If there is a default section with no pads put all unincluded pads in it
+    if let Some(index) = sections.iter().position(|x| x.pads.is_empty()) {
+        // Collect all pads mentioned so far
+        let mut pads_here: Vec<u8> = sections.iter().flat_map(|x| x.pads.clone()).collect();
+        if pads_here.len() < 64 {
+            // Need the default
+            pads_here.sort();
+            // Check each row for missing pads and add them to default
+            for r in 1..=8 {
+                let pads: Vec<&u8> = pads_here.iter().filter(|x| *x / 10 == r).collect();
+                for c in 1..=8 {
+                    let pad = r * 10 + c;
+                    if !pads.iter().any(|x| x == &&pad) {
+                        sections[index].pads.push(pad);
+                    }
+                }
+            }
+        }
+    }
+    Some(sections)
 }
+
+// Get a MIDI port that has a name containing `keyword`
+fn get_midi_port<T: midir::MidiIO>(midi_io: &T, keyword: &str) -> Option<T::Port> {
+    for port in midi_io.ports() {
+        let name = match midi_io.port_name(&port) {
+            Ok(name) => name,
+            Err(_) => continue,
+        };
+
+        if name.contains(keyword) {
+            eprintln!("Guessing port from keyword: {keyword} name: {name}");
+            return Some(port);
+        }
+    }
+
+    None
+}
+
+/// Create an output MIDI port to the LPX.
+/// It uses the passed parameter `name` to create a prort: LpxCtl:<name>
+fn get_midi_out(name: &str) -> Result<MidiOutputConnection, Box<dyn Error>> {
+    let midi_output = MidiOutput::new("LpxCtl")?;
+    let port = get_midi_port(&midi_output, "Launchpad X LPX MIDI In").unwrap(); //.ok_or(Err("Failed guess port".into())?);
+    Ok(midi_output.connect(&port, name)?)
+}
+
+/// Create a MIDI input port, connected from the LPX MIDI port.
+/// `name` is the port name for the created port
+/// `f` is the function that takes a channel and sends the MIDI that
+/// it wants to handle down that channel
+/// `tx` is the channel
+fn get_midi_in(
+    name: &str,
+    f: impl FnMut(u64, &[u8], &mut Sender<[u8; 3]>) + Send + 'static,
+    tx: Sender<[u8; 3]>,
+) -> Result<MidiInputConnection<Sender<[u8; 3]>>, Box<dyn Error>> {
+    let midi_input = MidiInput::new("LpxCtl")?;
+    let port = get_midi_port(&midi_input, "Launchpad X LPX MIDI In").unwrap();
+    //.ok_or(Err("Failed guess port".into())?);
+    let result = midi_input.connect(&port, name, f, tx)?;
+    Ok(result)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // The only argument is a configuration file
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Pass name of pad definitions, JSON formatted, file as sole argument");
+    }
+    let filename = &args[1];
+
+    // Initialise the collection of `Section` from the file. (See `section.rs`)
+    let sections: Vec<Section> = load_sections(filename).expect("Failed to load sections");
+
+    // The channel to send MIDI messages, received from the LPX in the
+    // MidiInputConnection, here to the main thread
+    let (tx, rx): (Sender<[u8; 3]>, Receiver<[u8; 3]>) = mpsc::channel::<[u8; 3]>();
+
+    // Connect to the LPX to receive pad press events.  `f` is the
+    // function that handles input MIDI and sends them back to themain
+    // thread
+    let f = move |_stamp, message: &[u8], tx: &mut Sender<[u8; 3]>| {
+        // let message = MidiMessage::from_bytes(message.to_vec());
+        if message.len() == 3 {
+            let m3: [u8; 3] = message.try_into().unwrap();
+            tx.send(m3).unwrap();
+        }
+    };
+    // The port stays open as long as `_in` is in scope
+    let _in = get_midi_in("read_input", f, tx.clone())?;
+
+    // Create an output port to the LPX for sending it colour.
+    let mut colour_port: MidiOutputConnection = get_midi_out("colour_port")?;
+
+    // Selecting Layouts (page 7 programmers manual).  127 => "Programmer Mode"
+    let msg: [u8; 9] = [240, 0, 32, 41, 2, 12, 0, 127, 247];
+    match colour_port.send(&msg) {
+        Ok(()) => (),
+        Err(err) => eprintln!("{err}: Failed to send msg to LPX: {msg:?}"),
+    };
+
+    let make_colour = |section: &Section, colour: [u8; 3]| -> Vec<u8> {
+        // Buid the MIDI command that sets the colours of all the pads
+        // in a section (they are all the same colour - part of what
+        // defines a section).  One long MIDI sysex message that sets
+        // many pads in one command
+
+        // "LED lighting SysEx message" programmer's mabual page 15
+        let mut colour_message: Vec<u8> = vec![240, 0, 32, 41, 2, 12, 3];
+        let pads: Vec<u8> = section.pads().to_vec();
+        for pad in pads.iter() {
+            colour_message.push(3); // RGB colour
+            colour_message.push(*pad); // Pad index
+            colour_message.extend(colour.to_vec()); // RGB tripple
+        }
+        colour_message.push(247); // End message
+        colour_message
+    };
+
+    // Initialise the colours
+    for section in sections.iter() {
+        let colour = make_colour(section, section.main_colour);
+        match colour_port.send(&colour) {
+            Ok(()) => (),
+            Err(err) => eprintln!("{err}: Cannot send colour: {colour:?}"),
+        };
+    }
+
+    // Establish the output that sends MIDI to whatever software will
+    // interpret the MIDI to create sound and MIDI controls to
+    // whatever interprets them.  An external programme will have to
+    // conmplete these setups as this programme does not know what
+    // they will be
+    let midi_out: MidiOutput = MidiOutput::new("LpxCtlNote")?;
+    let port_name = "port";
+    let mut midi_note_out_port: MidiOutputConnection = midi_out.create_virtual(port_name)?;
+
+    let midi_out: MidiOutput = MidiOutput::new("LpxCtlCtl")?;
+    let port_name = "port";
+    let mut midi_ctl_out_port: MidiOutputConnection = midi_out.create_virtual(port_name)?;
+    eprintln!("2 Virtual MIDI Output port 'LpxCtlNote:{port_name}' is open");
+    eprintln!("3 Virtual MIDI Output port 'LpxCtlCtl:{port_name}' is open");
+
+    // Main loop.
+    loop {
+        let message: [u8; 3] = match rx.recv() {
+            Ok(m) => m,
+            Err(err) => panic!("{}", err),
+        };
+        eprint!(
+            "MIDI: {:2x} {:2x} {:2x}.  ",
+            message[0], message[1], message[2]
+        );
+        if message[0] == 144 {
+            // All MIDI notes from LPX start with 144, for initial
+            // noteon and noteoff
+
+            // Find the section the pad is in
+            let pad: u8 = message[1];
+
+            if let Some(section) = sections.iter().find(|x| x.pad_in(pad)) {
+                // got the section for a pad
+
+                // Send out the note
+                let velocity = message[2];
+                let message: [u8; 3] = [message[0], section.midi_note, velocity];
+                eprintln!(
+                    "SEND NoteOn: Type: {:2x} Note: {:2x} Velocity: {:2x}",
+                    message[0], message[1], message[2]
+                );
+                midi_note_out_port.send(&message)?;
+
+                if velocity > 0 {
+                    // Note on
+                    // Set colour of section to "active_colour"
+                    let active_colour = make_colour(section, section.active_colour);
+                    colour_port.send(&active_colour).unwrap();
+                } else {
+                    // Note off
+                    // Restore the colour
+                    let main_colour = make_colour(section, section.main_colour);
+                    colour_port.send(&main_colour).unwrap();
+                }
+                continue;
+            }
+        } else if message[0] == 176 {
+            // A control signal
+            eprintln!(
+                "SEND Ctl: {:2x} {:2x} {:2x}",
+                message[0], message[1], message[2]
+            );
+            midi_ctl_out_port.send(&message).unwrap();
+        }
+    }
+    // Ok(())
+}
+//
---
+++ src/section.rs
@@ -0,0 +1,166 @@
+use std::collections::HashSet;
+// use crate::lpx_ctl_error::LpxCtlError;
+use serde::{Deserialize, Serialize};
+/// A `Section` is a collection of pads on a LPX that is grouped".
+/// All the pads in it are one colour and emit the same note
+#[allow(unused)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Section {
+    pub pads: Vec<u8>, // 11-88
+    pub main_colour: [u8; 3],
+    pub active_colour: [u8; 3],
+    pub midi_note: u8,
+}
+
+impl Section {
+    /// FIXME: This must validate and return an error for invalid values
+    #[allow(unused)]
+    pub fn new(pads: Vec<u8>, main_colour: [u8; 3], active_colour: [u8; 3], midi_note: u8) -> Self {
+        // -> Result<Self, LpxCtlError>
+        let result = Self {
+            pads,
+            main_colour,
+            active_colour,
+            midi_note,
+        };
+        if result.valid() {
+            // Ok(result)
+            result
+        } else {
+            // Err(LpxCtlError::invalid_section)
+            panic!("Invalid section");
+        }
+    }
+
+    /// Check the constraints on a `Section`:
+    /// Each pad in `pads` must be valid
+    /// There must be no repeats
+    /// There can be zero pads
+    fn valid(&self) -> bool {
+        !self.pads.iter().any(|x| {
+            !Self::valid_pad(*x)
+                || self // No repeats
+                    .pads
+                    .iter()
+                    .filter(|y| &x == y)
+                    .collect::<Vec<&u8>>()
+                    .len()
+                    != 1
+        }) && self.pads.len() <= 64
+    }
+
+    // Check that a `pad` is valid
+    fn valid_pad(pad: u8) -> bool {
+        (11..=88).contains(&pad) && pad % 10 != 0 && pad % 10 != 9
+    }
+
+    // Check a set of `Section` to see if they are valid as a grouop
+    pub fn check_sections(sections: &[Section]) -> bool {
+        // Can only be one section with no pads.  It is the default section
+        let default_section_count = sections
+            .iter()
+            .filter(|x| x.pads.is_empty())
+            .collect::<Vec<&Section>>()
+            .len();
+        let a = if default_section_count < 2 {
+            true
+        } else {
+            eprintln!("Too many ({default_section_count}) default sections");
+            return false;
+        };
+        // No intersections
+        let b = true;
+        for i in 0..(sections.len() - 1) {
+            for j in (i + 1)..sections.len() {
+                if sections[i].intersect(&sections[j]) {
+                    eprintln!(
+                        "section intersection: Sections:\n\t{}\n\t{}",
+                        sections[i], sections[j]
+                    );
+                    return false;
+                }
+            }
+        }
+        // There is a default section (with no pads) or every pad is
+        // in a section, exactly once
+        let c = default_section_count == 1 || {
+            let mut hs: HashSet<u8> = HashSet::new();
+            let mut v: Vec<u8> = Vec::new();
+            for s in sections.iter() {
+                for p in s.pads.iter() {
+                    hs.insert(*p);
+                    v.push(*p);
+                }
+            }
+            if hs.len() == v.len() && v.len() == 64 {
+                true
+            } else {
+                eprintln!("There are some pads in more than one section");
+                false
+            }
+        };
+        a && b && c
+    }
+
+    #[allow(dead_code)]
+    pub fn intersect(&self, other: &Self) -> bool {
+        for i in self.pads.iter() {
+            for j in other.pads.iter() {
+                if i == j {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn parse_json(input: &str) -> Option<Vec<Section>> {
+        //eprintln!("Parse:{input}");
+        let result: Vec<Section> = match serde_json::from_str(input) {
+            Ok(r) => r,
+            Err(err) => panic!("{err}"),
+        };
+        match Self::check_sections(&result) {
+            true => Some(result),
+            false => {
+                panic!("Sections check failed")
+            }
+        }
+    }
+    // pub fn row_col_to_pad(row: u8, col: u8) -> u8 {
+    //     row * 10 + col
+    // }
+    // pub fn pad_to_row(pad: u8) -> u8 {
+    //     pad / 10
+    // }
+    // pub fn pad_to_col(pad: u8) -> u8 {
+    //     pad % 10
+    // }
+    /// Detect if a pad on the LPX is in this section
+    pub fn pad_in(&self, pad: u8) -> bool {
+        self.pads.contains(&pad)
+    }
+
+    // Return all pad indexes in this section
+    pub fn pads(&self) -> &Vec<u8> {
+        &self.pads
+    }
+}
+
+use std::fmt;
+#[allow(unused)]
+impl fmt::Display for Section {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Section - Pads: {:?}  Main Colour:[{}, {}, {}], Active Colour: [{}, {}, {}]",
+            self.pads,
+            self.main_colour[0],
+            self.main_colour[1],
+            self.main_colour[2],
+            self.active_colour[0],
+            self.active_colour[1],
+            self.active_colour[2]
+        )
+    }
+}
